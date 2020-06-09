b5ac378908faaa723512d8c38322f8258496728a
Luu Tuan Nguyen <miketuannguyen@gmail.com>
2020-06-09 17:46:28 +0700

    add list and delete employee

--- src/views/EmployeePage/index.js
+++ src/views/EmployeePage/index.js
@@ -1,54 +1,89 @@
 import React from 'react'
 import styled from 'styled-components'
-import {Table} from 'antd'
+import {useSelector} from 'react-redux'
+import {Table, message, Button} from 'antd'
+import {api} from '../../services'
+import {DeleteOutlined, EditOutlined} from '@ant-design/icons'
 
 const Wrapper = styled.main`
   display: flex;
   justify-content: center;
   flex-direction: column;
   align-items: center;
-  position: absolute;
-  z-index: -1;
   margin-left: 100px;
+  position: absolute;
+  z-index: 1000;
 `
 
 const EmployeePage = () => {
-  const dataSource = [
+  const token = useSelector(state => state.admin.token)
+  const [data, setData] = React.useState([])
+
+  React.useEffect(() => {
+    const getData = async () => {
+      const {data} = await api.get('employee', undefined, token)
+      setData(data.payload)
+    }
+    getData()
+  }, [token])
+
+  const deleteEmployee = async (id) => {
+    const result = await api.delete(`employee/${id}`, undefined, token)
+    if (result.isSuccess) {
+      message.success('Delete successfully!')
+      const newData = data.filter((employee) => employee.id !== id)
+      setData(newData)
+    }
+    else {
+      message.error(result.error.message)
+    }
+  }
+
+  const columns = [
     {
-      key: '1',
-      name: 'Mike',
-      age: 32,
-      address: '10 Downing Street'
+      title: 'Employee ID',
+      dataIndex: 'id',
+      key: 'id',
+      align: 'center'
     },
     {
-      key: '2',
-      name: 'John',
-      age: 42,
-      address: '10 Downing Street'
-    }
-  ]
-  
-  const columns = [
+      title: 'Email',
+      dataIndex: 'email',
+      key: 'email'
+    },
     {
       title: 'Name',
       dataIndex: 'name',
       key: 'name'
     },
     {
-      title: 'Age',
-      dataIndex: 'age',
-      key: 'age'
-    },
-    {
-      title: 'Address',
-      dataIndex: 'address',
-      key: 'address'
+      title: 'Actions',
+      dataIndex: 'id',
+      key: 'id',
+      align: 'center',
+      render: (id) => (
+        <div>
+          <Button
+            type='primary'
+            onClick={() => deleteEmployee(id)}
+            shape='circle'
+            icon={<DeleteOutlined />}
+            size={10}
+          />&nbsp;&nbsp;
+          <Button
+            type='primary'
+            shape='circle'
+            icon={<EditOutlined />}
+            size={10}
+          />
+        </div>
+      )
     }
   ]
 
   return (
     <Wrapper>
-      <Table dataSource={dataSource} pagination={false} columns={columns} />
+      <Table dataSource={data} pagination={false} columns={columns} />
     </Wrapper>
   )
 }
